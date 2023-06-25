7565235ac749618ee3d488f44210423c3228cd44
Niket Agrawal <niket.agrawal90@gmail.com>
2023-06-25 16:04:12 +0530

    Changes to bean definition

--- src/main/java/com/niket/agrawal/springBootOpenApi/SpringBoot3WithOpenApi3Application.java
+++ src/main/java/com/niket/agrawal/springBootOpenApi/SpringBoot3WithOpenApi3Application.java
@@ -18,15 +18,14 @@ public class SpringBoot3WithOpenApi3Application {
 	}
 
 	@Bean
-	public OpenAPI springShopOpenAPI() {
+	public OpenAPI openApiInformation() {
 		Server localServer = new Server().url("http://localhost:8080").description("Localhost Server URL");
 		Contact contact = new Contact().email("niket.agrawal90@gmail.com").name("Niket Agrawal");
 		Info info = new Info().contact(contact).description("Spring Boot 3 + Open API 3")
 				.summary("Demo of Spring Boot 3 & Open API 3 Integration").title("Spring Boot 3 + Open API 3")
 				.version("V1.0.0").license(new License().name("Apache 2.0").url("http://springdoc.org"));
 
-		return new OpenAPI()
-				.info(info).addServersItem(localServer);
+		return new OpenAPI().info(info).addServersItem(localServer);
 	}
 
 }
