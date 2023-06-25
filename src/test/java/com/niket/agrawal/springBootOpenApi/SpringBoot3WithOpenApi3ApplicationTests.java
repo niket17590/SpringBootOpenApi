eab579a7e1f459e398fa42e6c9f7640ab23e371e
Niket Agrawal <niket.agrawal90@gmail.com>
2023-06-25 13:58:44 +0530

    Initial commit

---
+++ src/test/java/com/niket/agrawal/springBootOpenApi/SpringBoot3WithOpenApi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niket.agrawal.springBootOpenApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3WithOpenApi3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
